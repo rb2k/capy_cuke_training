271d7725cfe739a54f7a4be0165d8f78d09e64ca
Marc Seeger <mail@marc-seeger.de>
2012-07-16 13:10:44 +0200

    Remove trailing slash from app_host

--- sample_project/features/support/env.rb
+++ sample_project/features/support/env.rb
@@ -7,5 +7,5 @@ require "capybara/cucumber"
 Capybara.default_driver = :selenium
 
 #Usually, we would just use an environment variable for that (ENV['SOMETHING'])
-Capybara.app_host = 'http://www.acquia.com/'
+Capybara.app_host = 'http://www.acquia.com'
 World(Capybara)
